e76c0739037a7bff8ecb7bfe76a04336fbaee9c0
shashankyadav03 <shashankyadav97@yahoo.in>
2020-07-31 00:03:32 +0530

    Add

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Self from './Self.js';
 
 function App() {
   return (
@@ -18,8 +19,11 @@ function App() {
         >
           Learn React
         </a>
+        <hr/>
+        <Self />
       </header>
     </div>
+    
   );
 }
 
---
+++ portfolio/src/Self.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Self = () => {
+      return (
+          <div >
+            <h2>Welcome!</h2>
+            <p>This is Shashank!</p>
+          </div>
+      );
+    
+}
+export default Self;
